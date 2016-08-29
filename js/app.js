40de3d65423ff62cf12e1a5959716b1db4d455cf
Derlis López <Derlis Lpez>
2016-08-29 15:09:04 -0400

    Menun de Metodos Lineales home.html con Preloader

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var container = document.getElementById('splash');
+setTimeout(function() {
+    container.classList.add('cerrar');
+ // document.body.style.overflowY= "visible";// despueés de cargar le devolvemos el scroll
+}, 9000);
